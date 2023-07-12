9d78f25d74450f4168991b0a04a0248227a71bc3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-07-12 19:52:40 +0200

    Created History page

---
+++ pages/history.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import { Layout } from "@/components/layout";
+import { PageContent } from "@/components/page";
+
+export default function SettingsPage() {
+  return (
+    <Layout>
+      <Head>
+        <title>Passliss</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PageContent page="history">History</PageContent>
+    </Layout>
+  );
+}
