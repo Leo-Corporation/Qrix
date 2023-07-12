321115ea44b1db6449bcb5312acfa6308ed9e498
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-07-12 14:48:17 +0200

    Fixed issues with theme

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,8 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-[#8B2DF0] text-primary-foreground hover:bg-primary/90 font-bold",
+        default: "bg-[#8B2DF0] text-white hover:bg-primary/90 font-bold",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -19,14 +18,14 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         nav: "justify-start bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
-        nav2: "justify-start bg-transparent hover:bg-[#CCE7FF] dark:hover:bg-[#003666] data-[state=open]:bg-transparent",
+        nav2: "justify-start bg-transparent hover:bg-[#DDC1FB] dark:hover:bg-[#003666] data-[state=open]:bg-transparent",
         link: "justify-center bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
         destructiveghost:
           "justify-center bg-transparent hover:bg-red-100 dark:hover:bg-red-800 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent text-red-500",
         navselect:
           "justify-start bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
         navselect2:
-          "justify-start bg-[#CCE7FF] text-[#8B2DF0] hover:text-white hover:bg-[#8B2DF0] dark:bg-[#3B0872] dark:text-slate-100",
+          "justify-start bg-[#DDC1FB] text-[#8B2DF0] hover:text-white hover:bg-[#8B2DF0] dark:bg-[#3B0872] dark:text-slate-100",
       },
       size: {
         default: "h-10 px-4 py-2",
