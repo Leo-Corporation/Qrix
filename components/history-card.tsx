899ae06310a4fca59129c2e343b0669a73445e7f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-07-15 12:17:31 +0200

    Added the possibility to copy items in history
    Also added the possibility to save an item directly from history

--- components/history-card.tsx
+++ components/history-card.tsx
@@ -2,13 +2,16 @@ import { GeneratedItem } from "@/types/history";
 import bwipjs from "bwip-js";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { Copy16Regular, Save16Regular } from "@fluentui/react-icons";
+import saveAs from "file-saver";
 
 export default function HistoryItem(props: { item: GeneratedItem }) {
   const [url, setURL] = useState("");
   function genBarcode() {
     try {
       // The return value is the canvas element
-      let canvas = bwipjs.toCanvas("code", props.item);
+      let canvas = bwipjs.toCanvas("code" + props.item.text, props.item);
       setURL(canvas.toDataURL());
     } catch (e) {
       // `e` may be a string or Error object
@@ -18,9 +21,57 @@ export default function HistoryItem(props: { item: GeneratedItem }) {
   useEffect(() => {
     genBarcode();
   }, []);
+
+  function copyCanvasContentsToClipboard(
+    canvas: HTMLCanvasElement,
+    onDone: () => void,
+    onError: (err: Error) => void
+  ) {
+    canvas.toBlob((blob) => {
+      // check for null blob
+      if (blob) {
+        let data = [new ClipboardItem({ [blob.type]: blob })];
+        navigator.clipboard.write(data).then(
+          () => {
+            onDone();
+          },
+          (err) => {
+            onError(err);
+          }
+        );
+      } else {
+        // handle null blob case
+        onError(new Error("Blob is null"));
+      }
+    });
+  }
+  function copyBtn() {
+    let canvas: HTMLCanvasElement = document.getElementById(
+      "code" + props.item.text
+    ) as HTMLCanvasElement;
+    copyCanvasContentsToClipboard(
+      canvas,
+      () => {
+        console.log("Copied successfully");
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+  function saveBtn() {
+    let canvas = document.getElementById(
+      "code" + props.item.text
+    ) as HTMLCanvasElement;
+    canvas.toBlob(function (blob) {
+      if (blob) {
+        saveAs(blob, `${props.item.text}.png`);
+      }
+    });
+  }
   return (
     <div className="p-5 m-2 bg-white dark:bg-slate-800 shadow-md rounded-md flex items-center">
-      <canvas className="hidden" id="code"></canvas>
+      <canvas className="hidden" id={"code" + props.item.text}></canvas>
       <span>
         <Image
           width={150}
@@ -30,6 +81,22 @@ export default function HistoryItem(props: { item: GeneratedItem }) {
           alt={props.item.text}
         />
       </span>
+      <div className="flex space-x-2 m-4 mr-0">
+        <Button
+          onClick={copyBtn}
+          variant="outline"
+          className="h-auto px-2 py-1"
+        >
+          <Copy16Regular />
+        </Button>
+        <Button
+          onClick={saveBtn}
+          variant="outline"
+          className="h-auto px-2 py-1"
+        >
+          <Save16Regular />
+        </Button>
+      </div>
     </div>
   );
 }
