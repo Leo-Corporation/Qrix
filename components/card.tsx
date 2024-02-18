fd33d6ba717ba649d6071a3df9b9d4b94b49ef0a
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-02-18 10:52:51 +0100

    Fixed hover issue on home page (#289)

--- components/card.tsx
+++ components/card.tsx
@@ -11,14 +11,14 @@ export default function DashboardCard(props: CardProps) {
   return (
     <a
       href={props.link}
-      className="group m-3 flex w-[350px] items-center space-x-2 rounded-lg border border-accent-color bg-white p-5 text-black shadow-md transition-all hover:-translate-y-1 hover:shadow-lg dark:bg-slate-900 dark:text-white"
+      className="hover:shadow-accent-trans group m-3 grid w-[350px] grid-cols-[auto,1fr,24px] items-center space-x-2 rounded-lg border border-accent-color bg-white p-5 text-black shadow-md shadow-accent-color/25 transition-all hover:-translate-y-1 hover:shadow-lg hover:shadow-accent-color/20 dark:bg-slate-900 dark:text-white"
     >
       <p className="icon-f text-4xl text-accent-color">{props.icon}</p>
       <div>
         <h3 className="text-xl font-bold">{props.title}</h3>
         <p className="text-sm">{props.description}</p>
       </div>
-      <p className="transition-all group-hover:-mr-2">
+      <p className="transition-all group-hover:translate-x-1">
         <ChevronRight20Regular color="#8B2DF0" />
       </p>
     </a>
