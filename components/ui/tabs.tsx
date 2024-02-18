a6f8388bf5f58142c88c3521f58919d9b430af7c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-02-18 10:57:31 +0100

    Improved the UI of the Tabs component (#290)

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
+      "inline-flex h-10 items-center justify-center space-x-2 p-1",
       className,
     )}
     {...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-foreground data-[state=active]:shadow-sm dark:data-[state=active]:bg-black",
+      "inline-flex items-center rounded-md border border-transparent px-3 py-1.5 text-sm font-semibold text-accent-color transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-accent-color data-[state=active]:bg-accent-color/10 dark:data-[state=active]:bg-accent-color/20",
       className,
     )}
     {...props}
