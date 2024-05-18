550f177322172e5e292d60745ccd488b70a91023
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-05-18 12:24:17 +0200

    Added new Event QR Code type (#400)

--- pages/qrcode.tsx
+++ pages/qrcode.tsx
@@ -3,7 +3,6 @@ import { Layout } from "@/components/layout";
 import Head from "next/head";
 import { PageContent } from "@/components/page";
 import {
-  Calendar3Day20Regular,
   Checkmark16Regular,
   ChevronDown16Regular,
   Copy16Regular,
@@ -56,6 +55,8 @@ import { Textarea } from "@/components/ui/textarea";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { ContactInfo } from "@/lib/contact";
 import { RotateOption } from "@/types/rotate-type";
+import { CalendarEvent } from "@/lib/calendar-event";
+import { DatePicker } from "@/components/ui/date-picker";
 
 export default function BarcodePage() {
   const { t, lang } = useTranslation("common");
@@ -115,6 +116,13 @@ export default function BarcodePage() {
     website: "",
   });
 
+  const [event, setEvent] = useState<CalendarEvent>({
+    title: "",
+    description: "",
+    location: "",
+    start: "",
+    end: "",
+  });
   const handleInputChange = (event: {
     target: { value: SetStateAction<string> };
   }) => {
@@ -145,6 +153,9 @@ export default function BarcodePage() {
           break;
         case "contact":
           textContent = `BEGIN:VCARD\nVERSION:3.0\nN:${contact.lastName};${contact.firstName};\nFN:${contact.firstName} ${contact.lastName}\nORG:${contact.company}\nTEL;TYPE="cell,home":${contact.mobile}\nTEL;TYPE="fax,work":${contact.fax}\nTEL;TYPE="voice,home":${contact.phone}\nEMAIL:${contact.email}\nADR;TYPE=dom,home,postal,parcel:;;${contact.address.street};${contact.address.city};${contact.address.state};${contact.address.zip};${contact.address.country};\nTITLE:${contact.job}\nURL:${contact.website}\nEND:VCARD`;
+          break;
+        case "event":
+          textContent = `BEGIN:VEVENT\nSUMMARY:${event.title}\nDESCRIPTION:${event.description}\nLOCATION:${event.location}\nDTSTART:${event.start}\nDTEND:${event.end}\nEND:VEVENT`;
           break;
         default:
           textContent = content;
@@ -310,6 +321,9 @@ export default function BarcodePage() {
                     value="contact"
                   >
                     {t("contact")}
+                  </TabsTrigger>
+                  <TabsTrigger onClick={() => setTab("event")} value="event">
+                    {t("event")}
                   </TabsTrigger>
                 </span>
                 <span className="flex space-x-2">
@@ -628,6 +642,57 @@ export default function BarcodePage() {
                     className="col-span-2"
                     value={contact?.website}
                     placeholder={t("website")}
+                  />
+                </div>
+              </TabsContent>
+              <TabsContent value="event">
+                <div className="grid grid-cols-2 gap-y-1">
+                  <p>{t("event-title")}</p>
+                  <Input
+                    onChange={(v) => {
+                      let e = Object.create(event);
+                      e.title = v.target.value;
+                      setEvent(e);
+                    }}
+                    value={event.title}
+                    placeholder={t("event")}
+                  />
+
+                  <p>{t("description")}</p>
+                  <Textarea
+                    onChange={(v) => {
+                      let e = Object.create(event);
+                      e.description = v.target.value;
+                      setEvent(e);
+                    }}
+                    value={event.description}
+                    placeholder={t("description")}
+                  />
+                  <p>{t("location")}</p>
+                  <Input
+                    onChange={(v) => {
+                      let e = Object.create(event);
+                      e.location = v.target.value;
+                      setEvent(e);
+                    }}
+                    value={event.location}
+                    placeholder={t("location")}
+                  />
+                  <p>{t("start-date")}</p>
+                  <DatePicker
+                    setDate={(date: string) => {
+                      let e = Object.create(event);
+                      e.start = date;
+                      setEvent(e);
+                    }}
+                  />
+                  <p>{t("end-date")}</p>
+                  <DatePicker
+                    setDate={(date: string) => {
+                      let e = Object.create(event);
+                      e.end = date;
+                      setEvent(e);
+                    }}
                   />
                 </div>
               </TabsContent>
