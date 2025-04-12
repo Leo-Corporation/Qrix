7c1cce4dc391f8ab9495b7755a1574da41532e1c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-04-12 14:04:33 +0200

    Version 2.1.7.2504

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -105,7 +105,7 @@ export default function SettingsPage() {
   const [barRotation, setBarRotation] = useState<RotateOption>("N");
   const [qrRotation, setQrRotation] = useState<RotateOption>("N");
 
-  const ver = "2.1.6.2503";
+  const ver = "2.1.7.2504";
   function isSettings(object: any): object is Settings {
     return (
       typeof object === "object" &&
